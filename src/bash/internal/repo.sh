5880462ac7ef46c8874be9b6f9e9c657160f620e
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2015-05-10 17:50:48 +0200

    Apt-Get instal fail fix

--- src/bash/internal/repo.sh
+++ src/bash/internal/repo.sh
@@ -8,12 +8,23 @@ debfile="${deb}.deb"
 srcdir='/usr/src'
 debpath="${srcdir}/${debfile}"
 
+function getLastAptGetUpdate() {
+    local aptDate="$(stat -c %Y '/var/cache/apt/pkgcache.bin')"
+    local nowDate="$(date +'%s')"
+
+    echo $((nowDate - aptDate))
+}
+
+if [[ "${getLastAptGetUpdate}" -gt "$((24 * 60 * 60))" ]]; then
+  apt-get update -m
+fi
+
 if ! dpkg -l | grep -q 'ii  curl'; then
-	apt-get -y install curl
+	apt-get -y install curl || apt-get update -m && apt-get -y install curl
 fi
 if [ ! -f $debpath ]; then
 	stdbuf -oL -eL curl https://apt.puppetlabs.com/${debfile} -o ${debpath} 2>&1
 fi
 if ! dpkg -l | grep -q 'ii  puppetlabs-release'; then
-	dpkg -i $debpath && apt-get update
+	dpkg -i $debpath && apt-get update && rm -f $debpath
 fi
